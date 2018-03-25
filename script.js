ed25cc9a9535a6ae67d63514433f529b81af45b6
Rabea Gleissner <rabeagleissner@gmail.com>
2018-03-25 16:15:50 +0200

    Styling für Homepage

---
+++ script.js
@@ -0,0 +1,21 @@
+var titel = document.getElementById("ueber-mich");
+var werdegang = document.getElementById("werdegang");
+var ueberMich = document.getElementById("ueber-mich-heading");
+
+titel.addEventListener('click', function() {
+  if (werdegang.classList.contains("hide")) {
+    werdegang.classList.remove("hide");
+  } else {
+    werdegang.classList.add("hide");
+  }
+
+  if (ueberMich.classList.contains("zu")) {
+    console.log( 'here')
+    ueberMich.classList.remove("zu");
+    ueberMich.classList.add("auf");
+  } else {
+    ueberMich.classList.remove("auf");
+    ueberMich.classList.add("zu");
+  }
+}, false);
+
